c7a9ac75938634d9660d9005d91d419c2bbf19ef
Hari <hpriyav49@gmail.com>
2026-02-24 16:15:26 +0530

    Fixed port on render

--- app.py
+++ app.py
@@ -134,7 +134,9 @@ def logout():
     return redirect(url_for('login'))
 
 
+
 import os
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
